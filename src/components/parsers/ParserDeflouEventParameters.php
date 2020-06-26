eee0e2c047d810780b615660a9091502e2e6bcb4
jeyroik <jeyroik@gmail.com>
2020-06-26 15:17:53 +0300

    init

---
+++ src/components/parsers/ParserDeflouEventParameters.php
@@ -0,0 +1,69 @@
+<?php
+namespace deflou\components\parsers;
+
+use deflou\interfaces\applications\activities\IActivity;
+use extas\components\Item;
+use extas\interfaces\IHasDescription;
+use extas\interfaces\IHasValue;
+use extas\interfaces\parsers\IParseDispatcher;
+
+/**
+ * Class ParserDeflouEventParameters
+ *
+ * @package deflou\components\parsers
+ * @author jeyroik <jeyroik@gmail.com>
+ */
+class ParserDeflouEventParameters extends Item implements IParseDispatcher
+{
+    /**
+     * @param mixed $value
+     * @return mixed|string
+     */
+    public function __invoke($value)
+    {
+        preg_match_all('/\$"(.*?)"/', $value, $matches);
+        if (isset($this['event'])) {
+            /**
+             * @var IActivity $event
+             */
+            $event = $this['event'];
+            $parameters = $event->getParameters();
+            $byTitle = array_column(
+                $parameters,
+                IHasValue::FIELD__VALUE,
+                IHasDescription::FIELD__TITLE
+            );
+
+            if (!empty($matches[1])) {
+                return $this->replaceMatches($matches[1], $value, $byTitle);
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array $matches
+     * @param string $value
+     * @param array $byTitle
+     * @return string
+     */
+    protected function replaceMatches(array $matches, string $value, array $byTitle): string
+    {
+        foreach ($matches as $paramTitle) {
+            if (isset($byTitle[$paramTitle])) {
+                $value = str_replace('$"' . $paramTitle . '"', $byTitle[$paramTitle], $value);
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSubjectForExtension(): string
+    {
+        return 'deflou.parser.event.parameters';
+    }
+}
---
+++ tests/parsers/DeflouEventParametersTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace tests\parsers;
+
+use deflou\components\applications\activities\Activity;
+use deflou\components\parsers\ParserDeflouEventParameters;
+use extas\components\conditions\ConditionRepository;
+use extas\components\conditions\TSnuffConditions;
+use extas\components\parsers\Parser;
+use extas\components\repositories\TSnuffRepositoryDynamic;
+use extas\components\THasMagicClass;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DeflouEventParametersTest
+ *
+ * @package tests\parsers
+ * @author jeyroik <jeyroik@gmail.com>
+ */
+class DeflouEventParametersTest extends TestCase
+{
+    use THasMagicClass;
+    use TSnuffRepositoryDynamic;
+    use TSnuffConditions;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
+        $env->load();
+
+        $this->createSnuffDynamicRepositories([
+            ['parserRepository', 'name', Parser::class]
+        ]);
+
+        $this->registerSnuffRepos([
+            'conditionRepository' => ConditionRepository::class
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        $this->deleteSnuffDynamicRepositories();
+    }
+
+    public function testParsing()
+    {
+        $parser = new Parser([
+            Parser::FIELD__CLASS => ParserDeflouEventParameters::class,
+            Parser::FIELD__VALUE => '\$"(.*?)"',
+            Parser::FIELD__CONDITION => '#',
+            Parser::FIELD__PARAMETERS => [
+                'event' => new Activity([
+                    Activity::FIELD__PARAMETERS => [
+                        'test_current_status' => [
+                            'title' => 'Текущий статус теста',
+                            'name' => 'test_current_status',
+                            'value' => 'is ok'
+                        ]
+                    ]
+                ])
+            ]
+        ]);
+
+        $this->createSnuffCondition('regex');
+
+        $value = 'Текущий статус теста: $"Текущий статус теста".';
+        $this->assertTrue($parser->canParse($value), 'Can not parse string: ' . $value);
+        $this->assertEquals('Статус теста: is ok', $parser->parse($value));
+    }
+}
